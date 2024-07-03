afd7911f6fe4e187e4412cb388e512f2dff329d9
josueahadi <ahadihjosue@gmail.com>
2024-07-03 12:06:59 +0200

    static typing in typescript

--- netninja-tutorial/sandbox.js
+++ netninja-tutorial/sandbox.js
@@ -1,2 +1,6 @@
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
+var priceAfterDiscount = function (priceBeforeDiscount, discount) {
+    return priceBeforeDiscount - (priceBeforeDiscount * discount);
+};
+console.log(priceAfterDiscount(1000, 0.2));
--- netninja-tutorial/sandbox.ts
+++ netninja-tutorial/sandbox.ts
@@ -1,2 +1,8 @@
 const inputs = document.querySelectorAll('input');
 console.log(inputs);
+
+const priceAfterDiscount = (priceBeforeDiscount: number, discount: number) => {
+    return priceBeforeDiscount - (priceBeforeDiscount*discount);
+}
+
+console.log(priceAfterDiscount(1000, 0.2));
