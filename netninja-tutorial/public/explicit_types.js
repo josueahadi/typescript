790eb0cde5279cb1d2178d230bdb02da5f4052f1
josueahadi <ahadihjosue@gmail.com>
2024-07-07 22:43:03 +0200

    tsconfig

---
+++ netninja-tutorial/public/explicit_types.js
@@ -0,0 +1,21 @@
+"use strict";
+// explicit types 
+let character;
+let age;
+let isLoggedIn;
+//age = 'luigi';
+age = 20;
+// arrays 
+let arr = [];
+arr.push('shaun');
+// union types 
+let mixed = [];
+mixed.push('hello');
+mixed.push(5);
+//mixed.push(false)
+console.log(mixed);
+// objects
+let obj1;
+obj1 = { name: 'yoshi', age: 30 };
+let obj2;
+obj2 = { name: 'Mario', age: 20, beltColour: 'red' };
--- netninja-tutorial/public/sandbox.js
+++ netninja-tutorial/public/sandbox.js
@@ -1,6 +1,7 @@
-var inputs = document.querySelectorAll('input');
+"use strict";
+const inputs = document.querySelectorAll('input');
 console.log(inputs);
-var priceAfterDiscount = function (priceBeforeDiscount, discount) {
+const priceAfterDiscount = (priceBeforeDiscount, discount) => {
     return priceBeforeDiscount - (priceBeforeDiscount * discount);
 };
 console.log(priceAfterDiscount(1000, 0.2));
