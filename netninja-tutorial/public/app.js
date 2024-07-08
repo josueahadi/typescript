2261e99227310258e601ce70b4354acf77bd6b9c
josueahadi <ahadihjosue@gmail.com>
2024-07-08 15:49:21 +0200

    classes

--- netninja-tutorial/public/app.js
+++ netninja-tutorial/public/app.js
@@ -1,9 +1,26 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href)
+// classes 
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invoice1 = new Invoice('McKenon', 'consultancy', 10000);
+const invoice2 = new Invoice('Pearson', 'books', 200);
+console.log(invoice1, invoice2);
+let invoices = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+console.log(invoices);
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
+//console.log(form.children)
+// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
--- netninja-tutorial/src/app.ts
+++ netninja-tutorial/src/app.ts
@@ -1,10 +1,36 @@
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href)
+// classes 
+class Invoice {
+    client: string;
+    details: string;
+    amount: number; 
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d; 
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+
+const invoice1 = new Invoice('McKenon', 'consultancy', 10000);
+const invoice2 = new Invoice('Pearson', 'books', 200);
+console.log(invoice1, invoice2); 
+
+let invoices: Invoice[] = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+console.log(invoices);
+
+
 
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children)
+//console.log(form.children)
 
+// inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement; 
@@ -20,3 +46,4 @@ form.addEventListener('submit', (e: Event) => {
         amount.valueAsNumber
     )
 })
+
