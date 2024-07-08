826fbc9a7530f5c49cd93d4acdf47ed23255ef0c
josueahadi <ahadihjosue@gmail.com>
2024-07-08 14:58:28 +0200

    typescript casting

---
+++ netninja-tutorial/src/app.ts
@@ -0,0 +1,12 @@
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href)
+
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children)
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement; 
+const amount = document.querySelector('#amount') as HTMLInputElement; 
+
