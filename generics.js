96d9652bc356ccdff89b6e7b32fb0765fc4fc976
josueahadi <ahadihjosue@gmail.com>
2024-07-08 10:54:30 +0200

    Array<T>, Promise<T>

--- generics.js
+++ generics.js
@@ -4,3 +4,16 @@ function identity(arg) {
 }
 var output = identity("hello");
 console.log(output);
+// Array<T>
+var scores;
+scores = [78, 90, 56];
+console.log(scores);
+var coordinates;
+coordinates = [
+    [20, 89],
+    [67, 1900],
+    [34, 12]
+];
+// Promise<T>
+var promisedResponse = fetch("https://swapi.dev/api/");
+promisedResponse.then(function (res) { return console.log(res.ok); });
--- generics.ts
+++ generics.ts
@@ -6,3 +6,22 @@ function identity<T>(arg: T): T {
 
 let output = identity<string>("hello")
 console.log(output)
+
+// Array<T>
+
+let scores: Array<number>;
+scores = [78, 90, 56];
+console.log(scores)
+
+type Coordinate = [number, number];
+let coordinates: Array<Coordinate>;
+coordinates = [
+    [20, 89],
+    [67, 1900],
+    [34, 12]
+];
+
+// Promise<T>
+
+const promisedResponse: Promise<Response> = fetch("https://swapi.dev/api/");
+promisedResponse.then(res => console.log(res.ok))
