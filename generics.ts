65c77babaec96219f95ae4a7eec2ecb534b1eccd
josueahadi <ahadihjosue@gmail.com>
2024-07-08 11:10:04 +0200

    key value pairs

--- generics.ts
+++ generics.ts
@@ -25,3 +25,31 @@ coordinates = [
 
 const promisedResponse: Promise<Response> = fetch("https://swapi.dev/api/");
 promisedResponse.then(res => console.log(res.ok))
+
+// Record<K,V>
+
+type Result = {
+    firstName: string;
+    surname: string;
+    score: number;
+};
+//type ResultRecord = Record<string, Result>;
+type ResultRecord = Record<"rodj" | "janes" | "fredp", Result>;
+
+const records: ResultRecord = {
+    rodj: {
+        firstName: "Rod",
+        surname: "James",
+        score: 70,
+    },
+    janes: {
+        firstName: "Jane",
+        surname: "Smith",
+        score: 95,
+    },
+    fredp: {
+        firstName: "Fred",
+        surname: "Peters",
+        score: 60,
+    }
+}
