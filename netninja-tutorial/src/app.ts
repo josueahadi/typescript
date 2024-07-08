615109099ac04023bfa1f4213f299da385d9ec6c
josueahadi <ahadihjosue@gmail.com>
2024-07-08 15:04:11 +0200

    The DOM & Type Casting

--- netninja-tutorial/src/app.ts
+++ netninja-tutorial/src/app.ts
@@ -10,3 +10,13 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement; 
 const amount = document.querySelector('#amount') as HTMLInputElement; 
 
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
