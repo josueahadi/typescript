40966bc660401b092e583dc4da094adebeddf944
josueahadi <ahadihjosue@gmail.com>
2024-07-14 23:59:19 +0200

    typescript: create a  method to render a new 'li' to the container

---
+++ netninja-tutorial/src/classes/ListTemplate.ts
@@ -0,0 +1,9 @@
+export class ListTemplate {
+    constructor(private container: HTMLUListElement){}
+}
+
+/*
+1. register a list container (ul) in constructor
+2. create a render method to render a new 'li' to the container
+
+*/
